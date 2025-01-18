420ec8544f333ed0012ecea8fa5906cfc8064fd6
Jonatas Faria @QAJONATASMARTINS <jonatas1503@hotmail.com>
2025-01-18 02:13:26 +0000

    Upload dos primeiros arquivos  de exemplo do livro sobre testes de software 1 edição.

---
+++ 9.chapter-nine/tests/example-01.test.js
@@ -0,0 +1,18 @@
+const { describe, it } = require("mocha");
+
+/**
+ * O describe representa o nível mais alto do seu teste,
+ * aqui constumo utilizar o nome do SUT (Sistema sobre teste).
+ */
+describe("Produto XPTO", () => {
+  /**
+   * O arquivo it representa o seu teste.
+   * Para uma boa rastreabilidade é importante vincular o código de seu
+   * caso de teste criado na sua ferramenta de gerenciamento de teste.
+   */
+  it("[CT-0001] - Realizar o cadastro de pessoa física", () => {
+    /* Arrange */
+    /* Act */
+    /* Assert */
+  });
+});
