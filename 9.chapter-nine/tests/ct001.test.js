88d5c70b06d19f46e7a9698b48ed070b88d3d587
Jonatas Faria @QAJONATASMARTINS <jonatas1503@hotmail.com>
2025-01-18 15:35:51 +0000

    sugestões de nomenclaturas de exemplo para código de automação de testes.

---
+++ 9.chapter-nine/tests/ct001.test.js
@@ -0,0 +1,13 @@
+const { describe, it } = require("mocha");
+
+/**
+ * Regra de valor mínimo para saque em um sistema bancário, onde o valor mínimo de saque é 3.000 reais por dia.
+ * Usando o Test Design de Analise de valor limite para criar os casos de testes.
+ */
+describe("Produto XPTO", () => {
+  it("[CT-001] - Validar um saque de 2.999,99 (abaixo do limite)", () => {
+    /* Arrange (Organizar) */
+    /* Act (Agir) */
+    /* Assert (Afirmar)*/
+  });
+});
---
+++ 9.chapter-nine/tests/ct003.test.js
@@ -0,0 +1,13 @@
+const { describe, it } = require("mocha");
+
+/**
+ * Regra de valor mínimo para saque em um sistema bancário, onde o valor mínimo de saque é 3.000 reais por dia.
+ * Usando o Test Design de Analise de valor limite para criar os casos de testes.
+ */
+describe("Produto XPTO", () => {
+  it("[CT-003] - Validar um saque de 3.000,01 (acima do limite)", () => {
+    /* Arrange (Organizar) */
+    /* Act (Agir) */
+    /* Assert (Afirmar)*/
+  });
+});
