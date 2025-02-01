1e79b46d0434e5c950b342cba318805c0e59c47b
Jonatas Faria @QAJONATASMARTINS <jonatas1503@hotmail.com>
2025-02-01 21:45:42 +0000

    organização de pastas

---
+++ 9.chapter-nine/goodPractices/traceability/tests/example-02.test.js
@@ -0,0 +1,19 @@
+const { describe, it } = require("mocha");
+
+describe("Produto XPTO", () => {
+  it("[CT-0001] - Realizar login com credenciais válidas", () => {
+    /* ============================================================= */
+    /* Arrange (Organizar) */
+    /* 1. Passo: Abrir o navegador */
+    /* 2. Passo: Clicar no botão [Login] da página inicial */
+    /* ============================================================= */
+    /* Act (Agir) */
+    /* 3. Passo: Informar o e-mail */
+    /* 4. Passo: Informar a senha */
+    /* 5. Passo: Clicar no botão [Entrar] */
+    /* ============================================================= */
+    /* Assert (Afirmar)*/
+    /* 6. Passo: Checar se o nome do usuário logado é igual a "Usuário XPTO" */
+    /* ============================================================= */
+  });
+});
